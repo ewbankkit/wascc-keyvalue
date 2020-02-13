9a2690c9e87b2706f1bfa566035c0a4ea99fabcd
Kit Ewbank <Christopher.Ewbank@capitalone.com>
2020-02-13 13:19:58 -0500

    Run 'cargo fmt'.

--- src/kv.rs
+++ src/kv.rs
@@ -37,24 +37,24 @@ impl KeyValueStore {
     // Sets the value for a given key
     pub fn set(&mut self, key: &str, value: String) -> Result<(), Box<dyn Error>> {
         self.items
-        .entry(key.to_string())
-        .and_modify(|v| {
-            if let KeyValueItem::Scalar(_) = v {
-                *v = KeyValueItem::Scalar(value.clone())
-            }
-        })
-        .or_insert(KeyValueItem::Scalar(value));
+            .entry(key.to_string())
+            .and_modify(|v| {
+                if let KeyValueItem::Scalar(_) = v {
+                    *v = KeyValueItem::Scalar(value.clone())
+                }
+            })
+            .or_insert(KeyValueItem::Scalar(value));
 
         Ok(())
     }
 
     // Performs an atomic add operation, returning the new value
-    pub fn atomic_add(&self, key: &str, value: i32) ->  Result<i32, Box<dyn Error>> {
+    pub fn atomic_add(&self, key: &str, value: i32) -> Result<i32, Box<dyn Error>> {
         Ok(42)
     }
 
     // Adds a string value to a list stored within a given key.
-    pub fn list_add(&self, key: &str, item: &str) ->  Result<usize, Box<dyn Error>> {
+    pub fn list_add(&self, key: &str, item: &str) -> Result<usize, Box<dyn Error>> {
         Ok(42)
     }
 
@@ -69,7 +69,12 @@ impl KeyValueStore {
     }
 
     // Requests a list of values contained within a given key.
-    pub fn list_range(&self, key: &str, start: isize, stop_include: isize)-> Result<Vec<String>, Box<dyn Error>> {
+    pub fn list_range(
+        &self,
+        key: &str,
+        start: isize,
+        stop_include: isize,
+    ) -> Result<Vec<String>, Box<dyn Error>> {
         Ok(vec!["no".into()])
     }
 
@@ -79,7 +84,7 @@ impl KeyValueStore {
     }
 
     // Adds an item to a set.
-    pub fn set_add(&self, key: &str, value: &str)  -> Result<usize, Box<dyn Error>> {
+    pub fn set_add(&self, key: &str, value: &str) -> Result<usize, Box<dyn Error>> {
         Ok(42)
     }
 
@@ -89,22 +94,22 @@ impl KeyValueStore {
     }
 
     // Returns the union of sets indicated by list of keys.
-    pub fn set_union(&self, keys: Vec<String>)-> Result<Vec<String>, Box<dyn Error>> {
+    pub fn set_union(&self, keys: Vec<String>) -> Result<Vec<String>, Box<dyn Error>> {
         Ok(Vec::new())
     }
 
     // Returns the intersection of all sets indicated by the list of keys.
-    pub fn set_intersect(&self, keys: Vec<String>)-> Result<Vec<String>, Box<dyn Error>> {
+    pub fn set_intersect(&self, keys: Vec<String>) -> Result<Vec<String>, Box<dyn Error>> {
         Ok(Vec::new())
     }
 
     // Returns all members of a given set.
-    pub fn set_members(&self, key: &str)-> Result<Vec<String>, Box<dyn Error>> {
+    pub fn set_members(&self, key: &str) -> Result<Vec<String>, Box<dyn Error>> {
         Ok(Vec::new())
     }
 
     // Indicates whether or not a given key exists in the data store.
-    pub fn exists(&self, key: &str)-> Result<bool, Box<dyn Error>> {
+    pub fn exists(&self, key: &str) -> Result<bool, Box<dyn Error>> {
         Ok(false)
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-
 #[macro_use]
 extern crate wascc_codec as codec;
 
